40da46b71566869dcc9bb85af22b1206289b7047
remi.bezot <6487359+remi-bezot@users.noreply.github.com>
2026-06-03 21:34:52 +0200

    Update BlockRenderer.tsx

--- src/blocks/BlockRenderer.tsx
+++ src/blocks/BlockRenderer.tsx
@@ -1,8 +1,9 @@
-import { CardGridBlock } from "./CardGridBlock";
-import { HeroBlock } from "./HeroBlock";
-import { MailLinkBlock } from "./MailLinkBlock";
-import { PostListBlock } from "./PostListBlock";
-import { ParagraphBlock } from "./ParagraphBlock";
+import { CardGridBlock } from './CardGridBlock';
+import { HeroBlock } from './HeroBlock';
+import { MailLinkBlock } from './MailLinkBlock';
+import { ParagraphBlock } from './ParagraphBlock';
+import { PostListBlock } from './PostListBlock';
+import { PostMetaBlock } from './PostMetaBlock';
 
 type Props = {
   block: {
@@ -13,20 +14,23 @@ type Props = {
 
 export function BlockRenderer({ block }: Props) {
   switch (block.type) {
-    case "hero":
+    case 'hero':
       return <HeroBlock props={block.props} />;
 
-    case "paragraph":
+    case 'paragraph':
       return <ParagraphBlock props={block.props} />;
 
-    case "mail_link":
+    case 'mail_link':
       return <MailLinkBlock props={block.props} />;
 
-    case "card_grid":
+    case 'card_grid':
       return <CardGridBlock props={block.props} />;
 
-    case "post_list":
+    case 'post_list':
       return <PostListBlock props={block.props} />;
+
+    case 'post_meta':
+      return <PostMetaBlock props={block.props} />;
 
     default:
       return null;
